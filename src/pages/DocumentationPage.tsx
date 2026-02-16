19d12a8b6adbdc27400a4b3759aedda529ec276c
AairaSheikh <aairasheikh66@gmail.com>
2026-02-16 19:35:10 +0500

    feat(pages): add DocumentationPage with documentation section
    - Create new DocumentationPage component for displaying documentation content
    - Import and render DocumentationSection component
    - Provide dedicated page route for documentation access

---
+++ src/pages/DocumentationPage.tsx
@@ -0,0 +1,9 @@
+import DocumentationSection from '../components/DocumentationSection';
+
+export default function DocumentationPage() {
+  return (
+    <>
+      <DocumentationSection />
+    </>
+  );
+}
